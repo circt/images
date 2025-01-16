d61845a837b54e18b80d4782aa76228bae71ab89
Fabian Schuiki <fabian@schuiki.ch>
2025-01-15 17:26:36 -0800

    Add sanity checks for z3, yosys, and sby installations
    After installation, execute the tools on a simple example to check that
    they have been properly installed. This is especially useful for Yosys
    and SymbiYosys which depend on each other and z3, and other Python
    dependencies.

--- integration-test/sby.sh
+++ integration-test/sby.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # Downloads, compiles, and installs SymbiYosys
+set -xe
 
 YOSYS_VER=0.47
 
@@ -11,3 +12,23 @@ cd sby-yosys-$YOSYS_VER
 make PREFIX=/usr install
 cd ..
 rm -r sby-yosys-$YOSYS_VER
+
+# Perform a sanity check.
+cat > /tmp/sanity.v <<EOF
+module Foo(a);
+  input [7:0] a;
+  assert property (a * 2 == a + a);
+endmodule
+EOF
+
+sby <<EOF
+[options]
+mode prove
+
+[engines]
+smtbmc z3
+
+[script]
+read -formal /tmp/sanity.v
+prep -top Foo
+EOF
